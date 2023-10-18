63a976cfa4e8c1c3cc8e28391f8c39c5ece5dd4f
rene <renelorenz@gmx.net>
2023-10-18 21:48:36 +0200

    commit initial ALD solution, including current state of SLL

---
+++ SLL_TestApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLL_TestApp
+{
+
+  internal class Helicopter
+  {
+    int id;
+    public Helicopter(int id)
+    {
+      this.id = id;
+    }
+
+    public override bool Equals(object obj)
+    {
+      var heli = obj as Helicopter;
+      if (heli == null) return false;
+
+      return heli.id == this.id;
+    }
+
+  }
+
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      SinglyLinkedList.SinglyLinkedList<int> myList = new SinglyLinkedList.SinglyLinkedList<int>();
+      myList.Add(1);
+      myList.Add(2);
+      myList.Add(3);
+      myList.Add(3);
+      myList.Add(4);
+
+      bool exist = myList.Contains(3);
+
+      while (myList.Remove(3)) {}
+
+      exist = myList.Contains(3);
+
+      SinglyLinkedList.SinglyLinkedList<Helicopter> helis = new SinglyLinkedList.SinglyLinkedList<Helicopter>();
+      Helicopter a = new Helicopter(1);
+      Helicopter b = new Helicopter(2);
+
+      helis.Add(a);
+      helis.Add(b);
+
+      Helicopter c = new Helicopter(1);
+
+      var heliExist = helis.Contains(c);
+
+      Console.Read();
+
+    }
+  }
+}
---
+++ SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedList
+{
+  internal class Node<T>
+  {
+    //ctor
+    public Node(T item)
+    {
+      m_data = item;
+    }
+
+    public Node<T> m_next { get; set; }
+
+    public T m_data { get; set; }
+  }
+
+  public class SinglyLinkedList<T>
+  {
+    Node<T> m_head { get; set; }
+    Node<T> m_last { get; set; }
+    int m_cnt { get; set; }
+
+    public void Add(T item)
+    {
+      m_cnt++; //increment counter
+
+      Node<T> tmp = new Node<T>(item); //create new node
+      if(m_head == null)
+      {
+        //in case SSL is empty so far
+        m_head = tmp;
+        m_last = tmp;
+        return;
+      }
+
+      //else append at the end
+      m_last.m_next = tmp;
+      m_last = tmp;
+    }
+
+    public bool Contains(T item)
+    {
+      for (Node<T> i = m_head; i != null; i = i.m_next)
+      {
+        if (i.m_data.Equals(item))
+          return true;
+      }
+
+      return false;
+    }
+
+    public bool Remove(T item) 
+    {
+      Node<T> prev = m_head;
+      for (Node<T> i = m_head; i != null; i = i.m_next)
+      {
+        if (i.m_data.Equals(item))
+        {
+          prev.m_next = i.m_next;
+          return true;
+        }
+        prev = i;
+      }
+
+      return false;
+    }
+  }
+}
